229fd2f27c80c342d24ae6d3406bc5617bd0d7fe
bytewhisker <officialmdmahadi@gmail.com>
2026-05-05 19:16:06 +0600

    Allow wiwiusonepay iframe in CSP
    Polapine uses https://www.wiwiusonepay.com as external payment processor.
    Added to frame-src directive to allow iframes from this domain.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(helmet({
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "https://api.qrserver.com"],
-      frameSrc: ["'self'", "https://pay.polapine.com"],
+      frameSrc: ["'self'", "https://pay.polapine.com", "https://www.wiwiusonepay.com"],
       connectSrc: ["'self'", "https://pay.polapine.com", "https://ohkessuokmozfwldmqgs.supabase.co"]
     }
   }
